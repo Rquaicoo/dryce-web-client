3ebfb9dbc44ec13ac59f6a50b97f158e142491f3
Reu-Amissah <reujoyamissah@gmail.com>
2022-04-04 01:50:41 +0000

    add header nav

---
+++ src/components/Navbar/Header.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import './Header.css';
+import './Navlinks.js';
+import { LinkNames } from './Navlinks';
+import Buttons from '../Buttons/Buttons';
+
+class Header extends Component {
+
+  // clickState condition variables
+  state = {clicked: false}
+
+  handleClick = () => {
+      this.setState({ clicked: !this.state.clicked })
+  }
+
+  render () {
+    return (
+
+      <header>
+
+          <nav className="navBar">
+            <h1>DrYce</h1>
+
+            {/*menu bar with js condition==> ? clicked "classMenu" : "bar or X"*/}
+            <div className='navIcon' onClick={this.handleClick}>
+              <i class={this.state.clicked ? "fa-solid fa-bars" : "fa-solid fa-xmark"}></i>
+            </div>
+    
+            {/*nav links, imported from Navlinks.js #used Links*/}
+            <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                {LinkNames.map((link, index) =>{
+                  return (
+                    <li key={index}>
+                      <a className={link.cName} href={link.url}>
+                        {link.name}
+                      </a>
+                    </li>
+                  );
+                })}
+            </ul>{/*navLinks End*/}
+            
+            <Buttons 
+              title="Sign In" 
+              color={"white"} 
+              textColor={"black"} 
+              margin={'20px'} 
+              border={"2px"} 
+              bordercolor={"black"}>
+
+            </Buttons>
+            <Buttons title="Get Started" color={"rgb(9, 69, 231)"} textColor={"white"} margin={"50px"}></Buttons>
+          </nav>
+
+      </header>
+    )
+  }
+}
+
+export default Header
---
+++ src/components/Navbar/Navlinks.js
@@ -0,0 +1,22 @@
+export const LinkNames = [
+    {
+        name: "Home",
+        url: '#',
+        cName: 'navLink',
+    },
+    {
+        name: "Services",
+        url: '#',
+        cName: 'navLink',
+    },
+    {
+        name: "About Us",
+        url: '#',
+        cName: 'navLink',
+    },
+    {
+        name: "Contact Us",
+        url: '#',
+        cName: 'navLink',
+    },
+]
