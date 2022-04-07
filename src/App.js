33d3af3164041e89f2eb6eee9f78570b27929166
Reu-Amissah <reujoyamissah@gmail.com>
2022-04-07 16:08:41 +0000

    add Home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import Header from './components/Navbar/Header'
 // import Services from './components/Services/Services';
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 
 function App() {
   return (
-    // <Router className= "container">
-    //   <Routes>
-    //     <Route path="/" element={<Header />}/>
-    //     <Route path="*" element={<Header />}/>
-    //   </Routes>
-    // </Router>
+    
     <div className='container'>
         <Header/>
         {/* <Services/> */}
