f31d44342e6970f17d1570c2f33919fda1439132
Reu-Amissah <reujoyamissah@gmail.com>
2022-04-19 15:15:40 +0000

    merge conflicts fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import Header from './components/Navbar/Header'
-<<<<<<< HEAD
 import Services from './components/Services/Services';
 import About from './components/AboutUs/About'
 import Contact from  './components/Contact/Contact'
-=======
->>>>>>> da8a066c32f300cd47256fd17abcf14c6ca2d0f2
 
 
 function App() {
@@ -12,12 +9,9 @@ function App() {
     
     <div className='container'>
         <Header/>
-<<<<<<< HEAD
         <Services/>
         <About/>
         <Contact/>
-=======
->>>>>>> da8a066c32f300cd47256fd17abcf14c6ca2d0f2
     </div>
   );
 }
