100af463fbe6d04f2cff14b83a925ee98184c9d1
Reu-Amissah <reujoyamissah@gmail.com>
2022-04-19 15:04:05 +0000

    add contact us page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from './components/Navbar/Header'
-// import Services from './components/Services/Services';
+import Services from './components/Services/Services';
+import About from './components/AboutUs/About'
+import Contact from  './components/Contact/Contact'
+
 
 function App() {
   return (
     
     <div className='container'>
         <Header/>
-        {/* <Services/> */}
+        <Services/>
+        <About/>
+        <Contact/>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,16 +5,24 @@ import Buttons from '../Buttons/Buttons'
 
 const Home = () => {
   return (
-    <div className='container'>
+    <div className='container-home'>
         <div className='home-container'>
 
             <div className='home-description'>
-                <h1>Welcome to DRYCE laundry services</h1>
+                <h1>WELCOME TO DRYCE LAUNDRY SERVICES</h1>
                 <h3>
                     We OFFER top notch laundry services
                 </h3>
                 <p>We offer laudry services to all people across the country. Explore our services and get started today!</p>
-                <Buttons title="Explore Services" color={"rgb(9, 69, 231)"} textColor={"white"} bradius={"10px"}></Buttons>
+                <Buttons 
+                    title="Explore Services" 
+                    color={"rgb(9, 69, 231)"} 
+                    textColor={"white"} 
+                    bradius={"10px"} 
+                    shadow={"2px 2px 3px rgb(255, 189, 23)"}>
+                        
+                </Buttons>
+
             </div>
 
             <div className='home-img'>
