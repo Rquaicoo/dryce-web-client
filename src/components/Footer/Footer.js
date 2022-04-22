aae25a8a9c78b0c9592381f1b103afed8ebfe843
R. Amissah <r.amissah@Mr-Amisssahs-MacBook.local>
2022-04-22 01:09:22 +0000

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='container-footer'>
+        <div className='footer-container'>
+
+          <div className="footer-links"> 
+            <a href=''><i class="fa-brands fa-instagram"></i></a>
+            <a href=''><i class="fa-brands fa-facebook-square"></i></a>
+            <a href=''><i class="fa-brands fa-twitter"></i></a>
+            <a href=''><i class="fa-brands fa-github"></i></a>
+          </div>
+
+          <div className='footer-nav'>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Services</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Terms</a></li>
+              <li><a href="">Privacy Policy</a></li>
+            </ul>
+          </div>
+
+          <div className="footer-copyright">
+            <p>Copyright: &copy; 2022 <a href="" target="_blank">devOr</a></p>
+          </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
