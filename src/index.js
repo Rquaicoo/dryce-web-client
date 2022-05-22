a1c4ce2c674364d4f65e66c42aa65fa36e68d3c3
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-05-22 22:46:50 +0000

    registration and business form pages added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import BusinessRegistration from './components/Business-Details/BusinessRegistration';
+import SignInSide from './components/Authentication/SignIn';
+
 import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 
 ReactDOM.render(
   <Router className= "container">
       <Routes>
         <Route path="/" element={<App />}/>
+        <Route path="/business-registration" element={<BusinessRegistration />}/>
+        <Route path="/sign-in" element={<SignInSide />}/>
+
+        
         {/* <Route path="*" element={<Header />}/> */}
       </Routes>
     </Router>,
